f80ef80fa0cb56deb74ebdafaad469b6f43776e0
taizou <cactusgunman@gmail.com>
2013-02-06 12:35:33 +0000

    lib paths changed to reference copies in ../libs

--- devcpp/GEST_private.h
+++ devcpp/GEST_private.h
@@ -5,19 +5,19 @@
 #define GEST_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"0.2.1.227"
+#define VER_STRING	"0.2.1.233"
 #define VER_MAJOR	0
 #define VER_MINOR	2
 #define VER_RELEASE	1
-#define VER_BUILD	227
+#define VER_BUILD	233
 #define COMPANY_NAME	""
-#define FILE_VERSION	"0.2.1.227"
+#define FILE_VERSION	"0.2.1.233"
 #define FILE_DESCRIPTION	"unGEST GB emulator"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	"gest.exe"
 #define PRODUCT_NAME	"unGEST"
-#define PRODUCT_VERSION	"0.2.1.227"
+#define PRODUCT_VERSION	"0.2.1.233"
 
 #endif /*GEST_PRIVATE_H*/
