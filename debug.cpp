7b774951da074d8a0e5b702c775c3923d2dc3323
taizou <cactusgunman@gmail.com>
2013-02-10 17:13:06 +0000

    okay im going back to gpl v2, bc pretty much all existing emulators use v2 and i'd like to maintain compatibility. its "v2 or later" anyway so its still forwards compatible w/v3 LICENSING ugh

--- debug.cpp
+++ debug.cpp
@@ -5,20 +5,19 @@
    Based on GEST
    Copyright (C) 2003-2010 TM
 
-   This file is part of unGEST.
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
 
-   unGEST is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   unGEST is distributed in the hope that it will be useful,
+   This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with unGEST.  If not, see <http://www.gnu.org/licenses/>.
+   along with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 #include "debug.h"
 #include "config.h"
--- devcpp/GEST_private.h
+++ devcpp/GEST_private.h
@@ -5,19 +5,19 @@
 #define GEST_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"0.3.0.47"
+#define VER_STRING	"0.3.0.49"
 #define VER_MAJOR	0
 #define VER_MINOR	3
 #define VER_RELEASE	0
-#define VER_BUILD	47
+#define VER_BUILD	49
 #define COMPANY_NAME	""
-#define FILE_VERSION	"0.3.0.47"
+#define FILE_VERSION	"0.3.0.49"
 #define FILE_DESCRIPTION	"unGEST GB emulator"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	"ungest.exe"
 #define PRODUCT_NAME	"unGEST"
-#define PRODUCT_VERSION	"0.3.0.47"
+#define PRODUCT_VERSION	"0.3.0.49"
 
 #endif /*GEST_PRIVATE_H*/
