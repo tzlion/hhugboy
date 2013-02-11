1dca014a3228836f1a34070e2a0995e2595c2d47
taizou <cactusgunman@gmail.com>
2013-02-11 03:28:39 +0000

    little bit of renaming for a little bit of consistency

--- devcpp/GEST_private.h
+++ devcpp/GEST_private.h
@@ -5,19 +5,19 @@
 #define GEST_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"0.3.0.182"
+#define VER_STRING	"0.3.0.191"
 #define VER_MAJOR	0
 #define VER_MINOR	3
 #define VER_RELEASE	0
-#define VER_BUILD	182
+#define VER_BUILD	191
 #define COMPANY_NAME	""
-#define FILE_VERSION	"0.3.0.182"
+#define FILE_VERSION	"0.3.0.191"
 #define FILE_DESCRIPTION	"unGEST GB emulator"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
-#define ORIGINAL_FILENAME	"ungest.exe"
+#define ORIGINAL_FILENAME	"unGEST.exe"
 #define PRODUCT_NAME	"unGEST"
-#define PRODUCT_VERSION	"0.3.0.182"
+#define PRODUCT_VERSION	"0.3.0.191"
 
 #endif /*GEST_PRIVATE_H*/
