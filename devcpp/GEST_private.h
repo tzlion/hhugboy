885acece19dc119ad0481ffed13d89dc8e838ac7
taizou <cactusgunman@gmail.com>
2013-02-18 01:28:33 +0000

    added licensing notices to files that were missing them + changed version back to 1.0

--- devcpp/GEST_private.h
+++ devcpp/GEST_private.h
@@ -5,19 +5,19 @@
 #define GEST_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.0.0.710"
+#define VER_STRING	"1.0.0.711"
 #define VER_MAJOR	1
 #define VER_MINOR	0
 #define VER_RELEASE	0
-#define VER_BUILD	710
+#define VER_BUILD	711
 #define COMPANY_NAME	"hhug.me"
-#define FILE_VERSION	"1.0.0.710"
+#define FILE_VERSION	"1.0.0.711"
 #define FILE_DESCRIPTION	"hhugboy GB emulator"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	"taizou 2013"
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	"hhugboy.exe"
 #define PRODUCT_NAME	"hhugboy"
-#define PRODUCT_VERSION	"1.0.0.710"
+#define PRODUCT_VERSION	"1.0.0.711"
 
 #endif /*GEST_PRIVATE_H*/
