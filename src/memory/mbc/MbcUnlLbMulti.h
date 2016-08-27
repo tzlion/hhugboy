6b3bcd5ceafe74bd29dc17cb9e9d7cd1e97c5bf2
taizou <cactusgunman@gmail.com>
2016-08-27 21:10:42 +0100

    licensing

--- src/memory/mbc/MbcUnlLbMulti.h
+++ src/memory/mbc/MbcUnlLbMulti.h
@@ -1,6 +1,11 @@
-//
-// Created by Alex on 23/08/2016.
-//
+/*
+ * Additional mapper support for hhugboy emulator
+ * by taizou 2016
+ * This file released under Creative Commons CC0 https://creativecommons.org/publicdomain/zero/1.0/legalcode
+ *
+ * As part of the hhugboy project it is also licensed under the GNU General Public License v2
+ * See "license.txt" in the project root
+ */
 
 #ifndef HHUGBOY_MBCUNLLBMULTI_H
 #define HHUGBOY_MBCUNLLBMULTI_H
--- src/memory/mbc/MbcUnlLiCheng.h
+++ src/memory/mbc/MbcUnlLiCheng.h
@@ -1,6 +1,11 @@
-//
-// Created by Alex on 21/08/2016.
-//
+/*
+ * Additional mapper support for hhugboy emulator
+ * by taizou 2013-2016
+ * This file released under Creative Commons CC0 https://creativecommons.org/publicdomain/zero/1.0/legalcode
+ *
+ * As part of the hhugboy project it is also licensed under the GNU General Public License v2
+ * See "license.txt" in the project root
+ */
 
 #ifndef HHUGBOY_MBCUNLLICHENG_H
 #define HHUGBOY_MBCUNLLICHENG_H
