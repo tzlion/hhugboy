0e40d5a6675a8b7c5330d9bd7010ae6f04cd6e6c
taizou <cactusgunman@gmail.com>
2017-08-06 23:10:27 +0100

    makon old: proper rom size handling, support for rockman and mario simultaneously

--- src/memory/mbc/MbcUnlMakonOld.cpp
+++ src/memory/mbc/MbcUnlMakonOld.cpp
@@ -11,16 +11,24 @@ void MbcUnlMakonOld::writeMemory(unsigned short address, register byte data) {
         return;
     }
 
-    // Maybe this should extend MBC1 idk
-
     if (address >= 0x2000 && address <= 0x3FFF) {
 
         if (data == 0) data = 1; // MBC1 stylez
 
         if (isWeirdMode) {
-           //byte flippo[8] = {0,1,2,4,3,6,5,7}; // rocco
-            byte flippo[8] = {0,1,2,3,4,7,5,6}; // mario??????? doesnt work
-            data = switchOrder(data,flippo);
+            // okay so this detection is probably definitely hacky and this theory hasn't been tested on real carts but
+            // - Mario 3 and Rockman 8 each rely on different bit scrambling behaviour
+            // - But the actual cart mapper HW seems to be identical and the write used to enable the mode is the same
+            // So how does it know which mode to apply?
+            // The only difference in the multi initialisation for each game AFAIK is the ROM size
+            // So let's use that for now
+            if ((*gbRom)->ROMsize == 4) {
+                byte flippo[8] = {0,1,2,3,4,7,5,6}; // Mario
+                data = switchOrder(data,flippo);
+            } else {
+                byte flippo[8] = {0,1,2,4,3,6,5,7}; // Rockman
+                data = switchOrder(data,flippo);
+            }
         }
 
         rom_bank = data;
@@ -53,21 +61,30 @@ void MbcUnlMakonOld::writeMemory(unsigned short address, register byte data) {
                 // also unemulated: the actual change should not be effected until you do the write to 5002
                 break;
             case 0x02:
-                (*gbRom)->ROMsize = 0x04; // dubious but works 5now- we really need to map the actual size values here
                 if ((data & 0xF0) == 0xE0) {
-                    debug_print("Saving data in this game is not currently emulated");
-                   // (*gbRom)->RAMsize = 0x02;
-                   // (*gbRom)->battery = true;
-                } else {
-                  //  (*gbRom)->RAMsize = 0x00;
-                  //  (*gbRom)->battery = false;
+                    // indicates the game uses 8kb ram but we leave it always enabled for now
                 }
-                // also this write seems to specify if it needs save data or not (??)
+                switch(data & 0x0F) {
+                    case 0x00: // 512k
+                        (*gbRom)->ROMsize = 0x04;
+                        break;
+                    case 0x08: // 256k
+                        (*gbRom)->ROMsize = 0x03;
+                        break;
+                    case 0x0f: // 32k
+                        (*gbRom)->ROMsize = 0x00;
+                        break;
+                    default:
+                        // rom size seems to be 0x10 minus this value and and multiplied by 32kb
+                        // but the mappings defined above cover all observed cases so just fall back to 512k otherwise
+                        (*gbRom)->ROMsize = 0x04;
+                        break;
+                }
                 break;
             case 0x03:
                 isWeirdMode = (data & 0x10) == 0x10;
-                // unemulated behaviour: the change is actually effected immediately on the real cart
-                // e.g. the active rom bank changes w/o a further bankswitch required
+                // active rom bank should change at this point, no explicit switch required (observed on real cart)
+                writeMemory(0x2000, rom_bank);
                 break;
         }
         return;
@@ -88,6 +105,7 @@ MbcUnlMakonOld::MbcUnlMakonOld() :
 
 void MbcUnlMakonOld::resetVars(bool preserveMulticartState) {
     isWeirdMode = false;
+    (*gbRom)->ROMsize = 0x04;
     AbstractMbc::resetVars(preserveMulticartState);
 }
 
