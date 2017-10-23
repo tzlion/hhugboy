3e2d6a2ce8ace02b9f166aaafbdf9cca4a568b7b
taizou <cactusgunman@gmail.com>
2017-10-23 22:37:33 +0100

    split up loop from main function

--- src/main.cpp
+++ src/main.cpp
@@ -98,11 +98,156 @@ bool debug = false;
 
 int ramsize[10] = { 0, 2, 8, 32, 128, 64,64,64,8, 256 }; // KBytes
 
+int mainLoop()
+{
+    MSG msg;
+
+    LARGE_INTEGER cur_time;
+    LARGE_INTEGER now_time;
+    DWORD time_count = 16;
+    LARGE_INTEGER perf_cnt;
+    LARGE_INTEGER next_time;
+
+    int perf_flag = 0;
+    int time_to_sleep = 0;
+
+    if(QueryPerformanceFrequency(&perf_cnt))
+    {
+        perf_flag = 1;
+        time_count = (DWORD)(perf_cnt.QuadPart/59.7275);
+        QueryPerformanceCounter(&next_time);
+    } else
+    {
+        next_time.QuadPart = timeGetTime();
+        time_count = 33;
+    }
+
+    for(;;)
+    {
+        if(PeekMessage(&msg,NULL,0,0,PM_REMOVE))
+        {
+            if(msg.message == WM_QUIT)
+                break;
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        } else
+        if(!emulating || !GB1->romloaded || paused)
+        {
+            Sleep(50);
+        } else
+        {
+#ifdef ALLOW_DEBUG
+            if(debug && !paused && GB1->romloaded)
+         {
+            GB->frames = 1;
+            GB1->mainloop();
+            GB->frames = 0;
+
+            (renderer.*renderer.drawDebugScreen)();
+            continue;
+         }
+#endif
+
+            if(perf_flag)
+                QueryPerformanceCounter(&cur_time);
+            else
+                cur_time.QuadPart = timeGetTime();
+
+            //-----Reduce CPU usage----
+            if(options->reduce_cpu_usage && !speedup && cur_time.QuadPart < next_time.QuadPart)
+            {
+                QueryPerformanceCounter(&cur_time);
+
+                time_to_sleep = (int)floor(((next_time.QuadPart - cur_time.QuadPart)*1000) / perf_cnt.QuadPart); // Time to sleep (in milliseconds)
+                if(--time_to_sleep < 0)
+                    time_to_sleep = 0;
+
+                Sleep(time_to_sleep);
+
+                do
+                {
+                    //Sleep(0);
+
+                    QueryPerformanceCounter(&cur_time);
+                } while(cur_time.QuadPart < next_time.QuadPart);
+            }
+            //-----Reduce CPU usage----
+
+            if(speedup || cur_time.QuadPart >= next_time.QuadPart)
+            {
+                // Runs until frame
+                if(!multiple_gb)
+                {
+                    do
+                    {
+                        GB1->mainloop();
+                    } while(GB1->frames < 1);
+                } else
+                {
+                    do
+                    {
+                        GB = GB1;
+
+                        GB1->mainloop();
+
+                        GB = GB2;
+
+                        GB2->mainloop();
+                    } while(GB1->frames < 1);
+                }
+
+                GB1->frames = 0;
+
+                next_time.QuadPart = cur_time.QuadPart + time_count;
+
+                //-----Auto frameskip----
+                if(options->video_auto_frameskip)
+                {
+                    QueryPerformanceCounter(&cur_time);
+
+                    if(cur_time.QuadPart > next_time.QuadPart)
+                    {
+                        if(options->video_frameskip < 9)
+                        {
+                            ++options->video_frameskip;
+                        }
+                    }
+                    else if(options->video_frameskip > 0)
+                    {
+                        --options->video_frameskip;
+                    }
+
+                    if(multiple_gb)
+                        wsprintfW(w_title_text,L"%s - %s --- %s, frameskip: %i",w_emu_title,GB1->rom_filename,GB2->rom_filename,options->video_frameskip);
+                    else
+                        wsprintfW(w_title_text,L"%s - %s, frameskip: %i",w_emu_title,GB1->rom_filename,options->video_frameskip);
+                    SetWindowText(hwnd,w_title_text);
+                }
+                //-----Auto frameskip----
+
+                //-----Reduce CPU usage----
+                //  if(options->reduce_cpu_usage && !speedup /*&& (!multiple_gb || multiple_gb && current_gb == 2)*/)
+                /*   {
+                      QueryPerformanceCounter(&now_time);
+
+                      time_to_sleep = (int)floor(((next_time.QuadPart - now_time.QuadPart)*1000) / perf_cnt.QuadPart); // Time to sleep (in milliseconds)
+                      time_to_sleep -= 1; //Time to run sleep function+alpha
+
+                      if(time_to_sleep > 0)
+                         Sleep(time_to_sleep);
+                   }*/
+                //-----Reduce CPU usage----
+            }
+        }
+    }
+
+    return msg.wParam;
+}
+
 int WINAPI WinMain(HINSTANCE hThisInstance,HINSTANCE hPrevInstance, LPSTR  lpszArgument,int nFunsterStil)
 {
 
    WNDCLASSEX wincl;
-   MSG msg;
 
    /* The Window structure */
    hinst = hThisInstance;
@@ -124,8 +269,6 @@ int WINAPI WinMain(HINSTANCE hThisInstance,HINSTANCE hPrevInstance, LPSTR  lpszA
       return 0;
    
    emuMenu.init(hThisInstance);
-   
-
 
    hwnd = CreateWindowEx(0,w_szClassName,w_emu_title,WS_SIZEBOX|WS_OVERLAPPEDWINDOW,150,150,2*160,2*144,HWND_DESKTOP,emuMenu.getMenu(),hThisInstance,NULL);
    
@@ -153,26 +296,6 @@ int WINAPI WinMain(HINSTANCE hThisInstance,HINSTANCE hPrevInstance, LPSTR  lpszA
    }
    gb_system::gfx_bit_count = renderer.getBitCount();
    GB->init_gfx();
-        
-   LARGE_INTEGER cur_time;
-   LARGE_INTEGER now_time;
-   DWORD time_count = 16;
-   LARGE_INTEGER perf_cnt;
-   LARGE_INTEGER next_time;
-
-   int perf_flag = 0;
-   int time_to_sleep = 0;
-     
-   if(QueryPerformanceFrequency(&perf_cnt))
-   {
-      perf_flag = 1;
-      time_count = (DWORD)(perf_cnt.QuadPart/59.7275);
-      QueryPerformanceCounter(&next_time);
-   } else 
-   {
-      next_time.QuadPart = timeGetTime();
-      time_count = 33;
-   }
 
    //Get program directory
    wchar_t buffer[PROGRAM_PATH_SIZE];
@@ -239,123 +362,5 @@ int WINAPI WinMain(HINSTANCE hThisInstance,HINSTANCE hPrevInstance, LPSTR  lpszA
       }
    }
 
-   for(;;)
-   {
-      if(PeekMessage(&msg,NULL,0,0,PM_REMOVE)) 
-      {
-         if(msg.message == WM_QUIT)
-            break;
-         TranslateMessage(&msg);
-         DispatchMessage(&msg);
-      } else 
-      if(!emulating || !GB1->romloaded || paused)
-      {
-         Sleep(50);
-      } else
-      {        
-         #ifdef ALLOW_DEBUG                
-         if(debug && !paused && GB1->romloaded)
-         {
-            GB->frames = 1;
-            GB1->mainloop();
-            GB->frames = 0;
-
-            (renderer.*renderer.drawDebugScreen)();
-            continue;
-         }
-         #endif
-         
-         if(perf_flag)
-            QueryPerformanceCounter(&cur_time);
-         else
-            cur_time.QuadPart = timeGetTime();
-
-         //-----Reduce CPU usage----
-         if(options->reduce_cpu_usage && !speedup && cur_time.QuadPart < next_time.QuadPart)
-         {
-            QueryPerformanceCounter(&cur_time);
-
-            time_to_sleep = (int)floor(((next_time.QuadPart - cur_time.QuadPart)*1000) / perf_cnt.QuadPart); // Time to sleep (in milliseconds)
-            if(--time_to_sleep < 0)
-                time_to_sleep = 0;
-
-            Sleep(time_to_sleep);
-
-            do
-            {
-               //Sleep(0);
-
-               QueryPerformanceCounter(&cur_time);
-            } while(cur_time.QuadPart < next_time.QuadPart);
-         }
-         //-----Reduce CPU usage----
-
-         if(speedup || cur_time.QuadPart >= next_time.QuadPart)
-         {
-             // Runs until frame
-             if(!multiple_gb)
-             {
-               do
-               {
-                 GB1->mainloop();
-               } while(GB1->frames < 1);
-             } else
-             {
-               do
-               {
-                 GB = GB1;
-
-                 GB1->mainloop();
-
-                 GB = GB2;
-
-                 GB2->mainloop();
-               } while(GB1->frames < 1);
-             }
-
-             GB1->frames = 0;
-
-             next_time.QuadPart = cur_time.QuadPart + time_count;
-
-             //-----Auto frameskip----
-             if(options->video_auto_frameskip)
-             {
-                QueryPerformanceCounter(&cur_time);
-                
-                if(cur_time.QuadPart > next_time.QuadPart)
-                {
-                   if(options->video_frameskip < 9)
-                   {
-                      ++options->video_frameskip;
-                   }
-                }
-                else if(options->video_frameskip > 0)
-                {
-                   --options->video_frameskip;
-                }
-                
-                if(multiple_gb)
-                   wsprintfW(w_title_text,L"%s - %s --- %s, frameskip: %i",w_emu_title,GB1->rom_filename,GB2->rom_filename,options->video_frameskip);
-                else
-                   wsprintfW(w_title_text,L"%s - %s, frameskip: %i",w_emu_title,GB1->rom_filename,options->video_frameskip);
-                SetWindowText(hwnd,w_title_text);                
-             }
-             //-----Auto frameskip----
-
-             //-----Reduce CPU usage----
-           //  if(options->reduce_cpu_usage && !speedup /*&& (!multiple_gb || multiple_gb && current_gb == 2)*/)
-          /*   {
-                QueryPerformanceCounter(&now_time);
-                
-                time_to_sleep = (int)floor(((next_time.QuadPart - now_time.QuadPart)*1000) / perf_cnt.QuadPart); // Time to sleep (in milliseconds)
-                time_to_sleep -= 1; //Time to run sleep function+alpha
-                                             
-                if(time_to_sleep > 0)
-                   Sleep(time_to_sleep);
-             }*/
-             //-----Reduce CPU usage----                   
-          }  
-      }
-   }
-   return msg.wParam;
+   return mainLoop();
 }
