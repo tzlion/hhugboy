540cee77bd3e115ee55e83c37548f542482db836
taizou <cactusgunman@gmail.com>
2018-04-29 00:25:26 +0100

    improvements to memory searcher inc custom range

--- src/ui/memsearch/memsearch.cpp
+++ src/ui/memsearch/memsearch.cpp
@@ -13,13 +13,14 @@ using namespace std;
 extern HINSTANCE hinst;
 extern HWND hwnd;
 
-HWND megaBullshitDialog;
-HWND megaBullshitDialogHandle = NULL;
+HWND memorySearcherDialog;
+
+int searchCount = 0;
 
 void addMessage(const wchar_t* message)
 {
-    if (megaBullshitDialog) {
-        HWND hwndbox = GetDlgItem(megaBullshitDialog, ID_MEM_SEARCH_LOG);
+    if (memorySearcherDialog) {
+        HWND hwndbox = GetDlgItem(memorySearcherDialog, ID_MEM_SEARCH_LOG);
         SendMessage(hwndbox, LB_ADDSTRING, 0, (LPARAM)message );
         SendMessage(hwndbox, LB_SETCARETINDEX, SendMessage(hwndbox,LB_GETCOUNT,0,0)-1, true );
     }
@@ -32,51 +33,97 @@ void addMessage(const char* message)
     addMessage(wmessage);
 }
 
-void searchForValue(wchar_t* string)
+void searchForValue(wchar_t* string, wchar_t* fromString, wchar_t* toString)
 {
-    int fart = wcstol(string, nullptr, 16) & 0xff;
+    int searchVal = wcstol(string, nullptr, 16) & 0xff;
+    int searchFrom = wcstol(fromString, nullptr, 16) & 0xffff;
+    int searchTo = wcstol(toString, nullptr, 16) & 0xffff;
 
     char msg[420];
-    sprintf(msg, "Searching for %02x...", fart);
+    sprintf(msg, "Searching for %02X from %04X to %04X...", searchVal, searchFrom, searchTo);
     addMessage(msg);
 
-    bool found = false;
+    char addressesOnRow[16][5];
+    for(int y=0;y<16;y++) {
+        strcpy(addressesOnRow[y],"    ");
+    }
+    int rowPos = 0;
+    int count = 0;
 
-    for(int x=0;x<=0xffff;x++) {
-        if (GB1->memory[x] == fart) {
-            sprintf(msg, "Found %02x at %04x", fart, x);
-            addMessage(msg);
-            found = true;
+    for(int x=searchFrom;x<=searchTo;x++) {
+        if (GB1->memory[x] == searchVal) {
+            if (count == 0) {
+                addMessage(" ");
+                addMessage(" Found at locations:");
+                addMessage(" ");
+            }
+            count++;
+            sprintf(addressesOnRow[rowPos], "%04X", x);
+            rowPos++;
+            if (rowPos == 16) {
+                sprintf(msg, " %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s",
+                        addressesOnRow[0], addressesOnRow[1], addressesOnRow[2],  addressesOnRow[3],
+                        addressesOnRow[4],  addressesOnRow[5], addressesOnRow[6],  addressesOnRow[7],
+                        addressesOnRow[8], addressesOnRow[9], addressesOnRow[10],  addressesOnRow[11],
+                        addressesOnRow[12],  addressesOnRow[13], addressesOnRow[14],  addressesOnRow[15]
+                );
+                addMessage(msg);
+                for(int y=0;y<16;y++) {
+                    strcpy(addressesOnRow[y],"    ");
+                }
+                rowPos = 0;
+            }
         }
     }
-
-    if (!found) {
-        sprintf(msg, "%02x not found", fart);
+    if (rowPos > 0) {
+        sprintf(msg, " %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s",
+                addressesOnRow[0], addressesOnRow[1], addressesOnRow[2],  addressesOnRow[3],
+                addressesOnRow[4],  addressesOnRow[5], addressesOnRow[6],  addressesOnRow[7],
+                addressesOnRow[8], addressesOnRow[9], addressesOnRow[10],  addressesOnRow[11],
+                addressesOnRow[12],  addressesOnRow[13], addressesOnRow[14],  addressesOnRow[15]
+        );
         addMessage(msg);
     }
 
+    addMessage(" ");
+    if (count == 0) {
+        sprintf(msg, " Not found");
+        addMessage(msg);
+    } else {
+        sprintf(msg, " %d instances found", count);
+        addMessage(msg);
+    }
+    addMessage(" ");
+
 }
 
-BOOL CALLBACK MemorySearcherLogProc(HWND hwndDlg, UINT message, WPARAM wParam, LPARAM lParam)
+BOOL CALLBACK MemorySearcherDialogProc(HWND hwndDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    HWND hwndbox = GetDlgItem(hwndDlg, ID_MEM_SEARCH_LOG);
     switch (message)
     {
         case WM_INITDIALOG:
-            megaBullshitDialog = hwndDlg;
-            wchar_t str[100];
-            wsprintf(str,L"RUDIMENTARY MEMORY SEARCH ACTIVATED");
-
-            SendMessage(hwndbox, LB_ADDSTRING, 0, (LPARAM)str );
-
+            memorySearcherDialog = hwndDlg;
+            SetDlgItemTextW(hwndDlg, ID_MEM_SEARCH_BOX, L"00");
+            SetDlgItemTextW(hwndDlg, ID_MEM_SEARCH_START_BOX, L"0000");
+            SetDlgItemTextW(hwndDlg, ID_MEM_SEARCH_END_BOX, L"7FFF");
+            searchCount = 0;
             break;
         case WM_COMMAND:
             switch (LOWORD(wParam))
             {
                 case ID_MEM_SEARCH_BTN:
-                    wchar_t cheat_str[10];
-                    GetDlgItemText(hwndDlg, ID_MEM_SEARCH_BOX, cheat_str, 3);
-                    searchForValue(cheat_str);
+                    wchar_t valueStr[3];
+                    GetDlgItemText(hwndDlg, ID_MEM_SEARCH_BOX, valueStr, 3);
+                    wchar_t fromStr[5];
+                    GetDlgItemText(hwndDlg, ID_MEM_SEARCH_START_BOX, fromStr, 5);
+                    wchar_t toStr[5];
+                    GetDlgItemText(hwndDlg, ID_MEM_SEARCH_END_BOX, toStr, 5);
+                    if (searchCount > 0) {
+                        addMessage("=================================================================================");
+                        addMessage(" ");
+                    }
+                    searchCount++;
+                    searchForValue(valueStr, fromStr, toStr);
                     break;
                 case IDOK:
                 case IDCANCEL:
@@ -92,13 +139,6 @@ BOOL CALLBACK MemorySearcherLogProc(HWND hwndDlg, UINT message, WPARAM wParam, L
 
 void SpawnMemorySearcher()
 {
-    // Seems to rquire the .rc file being included in menu.rc ... For some reason like that is the Everything RC
-    // Maybe go like this:
-    // 1. One 'base RC' that only INCLUDES all the rest INCLUDING menu.
-    // 2. An 'Everything Header' that defines all the resource IDS
-    // -- OR MAYBE -- it only needs to define the first 2 bytes & then the second 2 can be defined per res.
-    //  NAH SOMETHING LIKE THE MENU WOULD OVERRUN THAT
-    // 3. The rest goes under its folders
-    megaBullshitDialogHandle = CreateDialog(hinst, MAKEINTRESOURCE(ID_MEM_SEARCH), hwnd, (DLGPROC)MemorySearcherLogProc);
-    ShowWindow(megaBullshitDialogHandle, SW_SHOW);
+    HWND memorySearcherDialogHandle = CreateDialog(hinst, MAKEINTRESOURCE(ID_MEM_SEARCH), hwnd, (DLGPROC)MemorySearcherDialogProc);
+    ShowWindow(memorySearcherDialogHandle, SW_SHOW);
 }
--- src/ui/memsearch/memsearch.h
+++ src/ui/memsearch/memsearch.h
@@ -7,8 +7,10 @@
 #define ID_MEM_SEARCH_LOG 198
 #define ID_MEM_SEARCH_BTN 197
 #define ID_MEM_SEARCH_BOX 196
+#define ID_MEM_SEARCH_START_BOX 195
+#define ID_MEM_SEARCH_END_BOX 194
 
-BOOL CALLBACK MemorySearcherLogProc(HWND hwndDlg, UINT message, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK MemorySearcherDialogProc(HWND hwndDlg, UINT message, WPARAM wParam, LPARAM lParam);
 void SpawnMemorySearcher();
 
 #endif
