d256618854a64b318bdc19073917410b4ff0bbb9
taizou <cactusgunman@gmail.com>
2019-05-18 22:45:05 +0100

    oh no

--- src/memory/mbc/MbcUnlLiCheng.cpp
+++ src/memory/mbc/MbcUnlLiCheng.cpp
@@ -12,8 +12,13 @@
 void MbcUnlLiCheng::writeMemory(unsigned short address, register byte data) {
     // Writes to certain addresses in the ROM bank switch range should be ignored
     // The carts do garbage writes to these addresses to select the wrong ROM banks if they're run as MBC5
-    if (address > 0x2000 && address < 0x3000 && (address & 0x80)) {
-        return;
+    if (address > 0x2000 && address < 0x3000) {
+        // different carts can be configured differently here, annoyingly
+        // e.g. tested zoids ignores writes with 0x80 set and cannon fodder ignores writes with 0x800
+        // i have not yet encountered a game that fails if you skip both, but...
+        if ((address & 0x80) || (address & 0x800)) {
+            return;
+        }
     }
     MbcNin5::writeMemory(address,data);
 }
