86cf383ebe7ba83484d5a5bbb1d76b84842111e3
NewRisingSun <8vytz1+dhp372pv94ebg@sharklasers.com>
2020-10-12 02:15:14 +0200

    Use dual license for additional mappers

--- src/memory/mbc/MbcUnlSachenMMC1.cpp
+++ src/memory/mbc/MbcUnlSachenMMC1.cpp
@@ -1,25 +1,11 @@
 /*
-   hhugboy Game Boy emulator
-   copyright 2013-2016 taizou
-   Based on GEST
-   Copyright (C) 2003-2010 TM
-   Incorporating code from VisualBoyAdvance
-   Copyright (C) 1999-2004 by Forgotten
-
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License
-   as published by the Free Software Foundation; either version 2
-   of the License, or (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation, Inc.,
-   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
+ * Additional mapper support for hhugboy emulator
+ * by NewRisingSun 2020
+ * This file released under Creative Commons CC0 https://creativecommons.org/publicdomain/zero/1.0/legalcode
+ *
+ * As part of the hhugboy project it is also licensed under the GNU General Public License v2
+ * See "license.txt" in the project root
+ */
 #include <cstdio>
 #include "MbcUnlSachenMMC1.h"
 #include "../../main.h"
