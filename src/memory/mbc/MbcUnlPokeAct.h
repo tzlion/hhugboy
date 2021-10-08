9e05bb41380715d5d385d60edf8955db6aa3ab22
taizou <cactusgunman@gmail.com>
2021-10-08 23:01:28 +0100

    update pokemon action implementation based on observations from actual cart

--- src/memory/mbc/MbcUnlPokeAct.h
+++ src/memory/mbc/MbcUnlPokeAct.h
@@ -1,16 +1,22 @@
-
+/*
+ * Additional mapper support for hhugboy emulator
+ * by taizou 2021
+ * This file released under Creative Commons CC0 https://creativecommons.org/publicdomain/zero/1.0/legalcode
+ *
+ * As part of the hhugboy project it is also licensed under the GNU General Public License v2
+ * See "license.txt" in the project root
+ */
 #ifndef HHUGBOY_MBCUNLPOKEACT_H
 #define HHUGBOY_MBCUNLPOKEACT_H
 
 #include "MbcNin5.h"
 
-class MbcUnlPokeAct : public MbcNin5 {
+// Simple protection only known to be used by 口袋怪獸-動作篇 with HK0819 pcb
 
+class MbcUnlPokeAct : public MbcNin5 {
     public:
         virtual byte readMemory(unsigned short address) override;
         virtual void writeMemory(unsigned short address, register byte data) override;
-
 };
-
 
 #endif //HHUGBOY_MBCUNLPOKEACT_H
