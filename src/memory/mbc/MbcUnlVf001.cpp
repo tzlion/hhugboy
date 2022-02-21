1ad755b5f24ba2fadf501c80f66efbefe070c938
taizou <cactusgunman@gmail.com>
2022-02-21 21:19:50 +0000

    note for a config thing

--- src/memory/mbc/MbcUnlVf001.cpp
+++ src/memory/mbc/MbcUnlVf001.cpp
@@ -59,7 +59,7 @@ void MbcUnlVf001::writeMemory(unsigned short address, byte data) {
                 debug_win("Protection config mode enabled");
             }
             configMode = true;
-            runningValue = 00;
+            runningValue = 0; // 0x10 for emocheng2.. SL-004 PCB
             return;
         }
 
